6ceb3cae850ab2dfc85713f410ac55acc653188c
Andreas Marienborg <andreas.marienborg@gmail.com>
2010-11-30 12:24:52 +0700

    Switch to better loading of libs.
    We now look in directories defined in JSINC for libs. This makes it easier to use installed version alongside checked out versions. Akin to PERL5INC

--- qunit.js
+++ qunit.js
@@ -1,5 +1,10 @@
-load("../qunit/qunit/qunit.js");
-load("../qunit-tap/lib/qunit-tap.js");
+/* We want to be able to look in different places! */
+var inc = new Array();
+
+init_inc(inc);
+
+safe_load("qunit.js");
+safe_load("qunit-tap.js");
 
 QUnit.init();
 QUnit.config.blocking = false;
@@ -14,7 +19,7 @@ function diag(msg) {
 }
 
 function bootstrap(file) {
-    load("../env-js/dist/env.rhino.js");
+    safe_load("env.rhino.js");
     Envjs({
         log: function(string) {
             diag(string);
@@ -26,4 +31,59 @@ function bootstrap(file) {
     });
     window.location = file;
 }
+function init_inc(inc) {
+    // What do we have here?
+    var cp = environment['java.class.path'];
+    // java.class.path: /Users/andremar/Projects/js/rhino/js.jar
+    if (cp.match('/usr/local/share/rhino')) {
+        // XXX: I hate hardcoding these, but blah!
+        inc.push('/usr/local/share/startsiden-javascript-qunit');
+        inc.push('/usr/local/share/startsiden-javascript-envjs');
+    }
 
+    var paths = getinc();
+    for (i in paths) {
+        inc.push(paths[i]);
+    }
+}
+function safe_load(lib) {
+    var found = 0;
+    for (dir in inc) {
+        // Need to check if our lib exists here
+        // XXX: Not working on windows with the whole / biz
+        dir = inc[dir];
+        if (!dir.match(environment['file.separator'] + "$")) {
+            dir = dir + environment['file.separator'];
+        }
+        var file = dir + lib;
+
+        if (!runCommand("test", "-f", file)) {
+            found++;
+            load(file);
+            // XXX: SHould exit loop here
+        }
+    }
+    if (found == 0) {
+        print("Could not find " + lib + " in our INC(" + inc.join(":") + ")");
+    } else if (found > 1) {
+        print("We found more than one version of "
+                + lib + " in our INC(" + inc.join(":") + ")");
+    }
+}
+function getinc() {
+    var path = getenv("JSINC");
+    return path.split(':');
+}
+function getenv(key) {
+    var opt = {'output': ''};
+    // XXX: does not work on windows!
+    if (environment['os.name'].match('Mac OS X|Linux')) {
+        runCommand('printenv', key, opt);
+        print(opt.output);
+    } else {
+        print("Unsupported os.name: " + environment['os.name']);
+        quit();
+    }
+    var out = opt.output.replace("\n", "");
+    return out;
+}
