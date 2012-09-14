76e037b734dc7c8bd48cb068bb0d7683e0616634
dagfinnr <dagfinn@startsiden.no>
2012-09-14 14:21:22 +0200

    Moving find_test_lib() to the new runner class

--- lib/Startsiden/Test/JavaScript.pm
+++ lib/Startsiden/Test/JavaScript.pm
@@ -29,8 +29,8 @@ use Class::Load qw();
 use Capture::Tiny;
 
 sub js_live_test {
-
     my ($type, $app, $url) = @_;
+    my $runner = Startsiden::Test::JavaScript::Base->new();
 
     if ($type eq 'cat') {
 
@@ -53,16 +53,17 @@ sub js_live_test {
 
         _run_psgi($psgi, $url);
     } elsif (!$app and !$url) {
-        _run_os_command(find_test_lib(), $type); # Only one arg, must be url!
+        _run_os_command($runner->find_test_lib(), $type); # Only one arg, must be url!
     } else {
         die "We do not support type $type together with app $app and url $url yet :/";
     }
 }
 sub js_test {
     my ($content) = @_;
+    my $runner = Startsiden::Test::JavaScript::Base->new();
     my @argv;
 
-    push(@argv, find_test_lib());
+    push(@argv, $runner->find_test_lib());
     my $f;
     if ($content and $content !~ /\n/ and -f $content) {
         # Content is a file :p
@@ -82,6 +83,7 @@ sub js_test {
 
 sub _run_psgi {
     my ($psgi, $url) = @_;
+    my $runner = Startsiden::Test::JavaScript::Base->new();
     $Plack::Test::Impl = 'Server' unless $ENV{PLACK_TEST_IMPL};
     $ENV{PLACK_SERVER} ||= 'HTTP::Server::Simple';
     my $app = Plack::Builder::builder {
@@ -95,7 +97,7 @@ sub _run_psgi {
     test_psgi $app, sub {
         my @argv;
 
-        push(@argv, find_test_lib(), shift->(GET $url)->base);
+        push(@argv, $runner->find_test_lib(), shift->(GET $url)->base);
         _run_os_command(@argv);
     };
 }
@@ -120,32 +122,4 @@ sub _generate_command {
     return $cmd;
 }
 
-sub find_test_lib {
-    # XXX: Need to support devel checkouts as well
-
-    my $f = 'startsiden-test.js';
-    my $dir;
-    try {
-        $dir = dir(dist_dir('Startsiden-Test-JavaScript'));
-    };
-    unless ($dir and -f $dir->file($f)) {
-        # XXX: Argh, I hate this.
-        my $pkg = __PACKAGE__ . ".pm";
-
-        $pkg =~ s|::|/|g;
-        my $file = file($INC{$pkg});
-        $dir = $file->dir;
-        while ($dir->parent ne $dir) {
-            if (-d $dir->subdir('share')) {
-                $dir = $dir->subdir('share');
-                last;
-            }
-            $dir = $dir->parent;
-        }
-    }
-    unless (-f $dir->file($f)) {
-        croak("Could not locate $f");
-    }
-    return $dir->file($f);
-}
 1;
--- lib/Startsiden/Test/JavaScript/Base.pm
+++ lib/Startsiden/Test/JavaScript/Base.pm
@@ -1,3 +1,35 @@
 package Startsiden::Test::JavaScript::Base;
 use Moose;
+use File::ShareDir qw(dist_dir);
+use Try::Tiny;
+use File::Temp qw(tempfile);
+use Path::Class;
+sub find_test_lib {
+    # XXX: Need to support devel checkouts as well
+
+    my $f = 'startsiden-test.js';
+    my $dir;
+    try {
+        $dir = dir(dist_dir('Startsiden-Test-JavaScript'));
+    };
+    unless ($dir and -f $dir->file($f)) {
+        # XXX: Argh, I hate this.
+        my $pkg = __PACKAGE__ . ".pm";
+
+        $pkg =~ s|::|/|g;
+        my $file = file($INC{$pkg});
+        $dir = $file->dir;
+        while ($dir->parent ne $dir) {
+            if (-d $dir->subdir('share')) {
+                $dir = $dir->subdir('share');
+                last;
+            }
+            $dir = $dir->parent;
+        }
+    }
+    unless (-f $dir->file($f)) {
+        croak("Could not locate $f");
+    }
+    return $dir->file($f);
+}
 1;
